e6a8df33a66eaa35c193be687d664909c61c02b0
MConfuse <mrconfus3@gmail.com>
2023-04-19 00:04:37 +0200

    V0.3 PRE 2
    Refactored AutoItemSwitchUtilities#getSlotWithMatchingOrEqualItemFromInventory and split the algorithm into two different sub algorithms to make it easier to decipher.
    
    Cleaned ItemStackMixin.java up a bit

--- src/main/java/de/confue/autoitemreplace/client/AutoItemSwitchUtilities.java
+++ src/main/java/de/confue/autoitemreplace/client/AutoItemSwitchUtilities.java
@@ -9,39 +9,82 @@ import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
+import org.jetbrains.annotations.NotNull;
 
 public class AutoItemSwitchUtilities
 {
 
 	/**
-	 * @param oldItemStack The item stack that should be searched for
-	 * @return The slot that item was found in, or -1 if nothing was found
+	 * @param oldItemStack The {@link ItemStack} that should be searched for
+	 * @return The slot that {@link Item} was found in, or -1 if nothing was found
 	 */
 	public static int getSlotWithMatchingOrEqualItemFromInventory(ItemStack oldItemStack)
 	{
+		ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
 		if (oldItemStack == null)
 		{
 			System.err.println("AutoItemSwitchUtilities.getSlotWithMatchingOrEqualItemFromInventory:");
 			System.err.println("The item that was used up / broke is null?");
 			return -1;
 		}
+		else if (player == null)
+		{
+			System.err.println("AutoItemSwitchUtilities.getSlotWithMatchingOrEqualItemFromInventory");
+			System.err.println("ClientPlayerEntity was null! Is this a server?");
+			return -1;
+		}
 
-		ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
 		PlayerInventory inventory = player.getInventory();
-		DefaultedList<ItemStack> main = inventory.main;
 		Item oldItem = oldItemStack.getItem();
-		/* Array Structure:	0: enchantLevel,	1: slotIndex */
-		final int[] itemSlots = {-1, -1}; // initializing everything to -1
 		final boolean isTool = oldItem instanceof MiningToolItem; // Tools are sorted by efficiency enchants
 		final boolean isSilkTouch = EnchantmentHelper.hasSilkTouch(oldItemStack);
+
+		if (isTool)
+		{
+			((MiningToolItem)oldItem).getMaterial().getMiningLevel();
+		}
 
 		final boolean isMeleeItem = oldItem instanceof SwordItem; // Swords are sorted by sharpness enchants
 //		final boolean isRangedItem = oldItemStack instanceof RangedWeaponItem;
 
-		System.out.println("isTool = " + isTool);
-		System.out.println("isSilkTouch = " + isSilkTouch); // TODO: Fix silk touch
+		int slot = getInventorySlotOfSameItem(inventory, oldItem, isTool, isSilkTouch, isMeleeItem);
 
-		// TODO: How do we manage silk touch? Surely not by replacing a silk pickaxe with a non silk one if it breaks
+		// found an item of the same type
+		if (slot != -1)
+		{
+			return slot;
+		}
+
+//		// if [1] is not -1 the slot shouldn't be either
+//		if (itemSlots[1] != -1)
+//		{
+//			return itemSlots[1];
+//		}
+
+		/*
+		Pickaxe order: Same level | Already searched for -> Netherite -> Diamond -> etc
+		 */
+
+		return getInventorySlotOfSimilarItem(inventory, oldItem, isTool, isSilkTouch, isMeleeItem);
+	}
+
+	/**
+	 * @param inventory   The inventory of the {@link net.minecraft.entity.player.PlayerEntity}
+	 * @param oldItem     The {@link Item} (type) to look for
+	 * @param isTool      Is the oldItem a tool?
+	 * @param isSilkTouch Is the item enchanted with silk touch?
+	 * @param isMeleeItem Is the item a melee weapon?
+	 * @return -1 or the index of the next best item of the same type/level
+	 */
+	private static int getInventorySlotOfSameItem(@NotNull PlayerInventory inventory, Item oldItem,
+												  boolean isTool, boolean isSilkTouch, boolean isMeleeItem)
+	{
+		DefaultedList<ItemStack> main = inventory.main;
+
+		/* Array Structure:	0: enchantLevel,	1: slotIndex */
+		final int[] itemSlots = {-1, -1}; // initializing everything to -1
 
 		/*
 		Order of search !Excludes any crafting/armor/offhand slot!:
@@ -73,15 +116,9 @@ public class AutoItemSwitchUtilities
 					{
 						/* If so, is the one we are looking at silk touch too? */
 						if (EnchantmentHelper.hasSilkTouch(stack))
-						{
-							System.out.println("silk");
 							enchantLevel = EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack);
-						}
 						else
-						{
-							System.out.println("no silk");
 							continue; // no? next!
-						}
 					}
 					else if (!EnchantmentHelper.hasSilkTouch(stack))
 						enchantLevel = EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack);
@@ -107,29 +144,106 @@ public class AutoItemSwitchUtilities
 					itemSlots[1] = i;
 				}
 
-//				sameItemSlot = i;
-				// break
 			}
 
 		}
 
-		// if [0] is not -1 the slot shouldn't be either
-		if (itemSlots[0] != -1)
-		{
-			return itemSlots[1];
-		}
-
-		/*
-		Pickaxe order: Same level -> Netherite -> Diamond -> etc
-		 */
-
-		// TODO: Search for other items of the same category
-
-
-		return -1;
+		/* should be either -1 still or actually be a slot in the inventory that has the next best alternative to the
+		 broken/used item */
+		return itemSlots[1];
 	}
 
-	public static Identifier getItemIdentifier(Item item)
+	/**
+	 * @param inventory   The inventory of the {@link net.minecraft.entity.player.PlayerEntity}
+	 * @param oldItem     The {@link Item} (type) to look for
+	 * @param isTool      Is the oldItem a tool?
+	 * @param isSilkTouch Is the item enchanted with silk touch?
+	 * @param isMeleeItem Is the item a melee weapon?
+	 * @return -1 or the index of the next best item of the same type (being a block/food)/level of that tool/weapon
+	 * that is the highest level available, with the highest efficiency
+	 */
+	private static int getInventorySlotOfSimilarItem(@NotNull PlayerInventory inventory, Item oldItem,
+													 boolean isTool, boolean isSilkTouch, boolean isMeleeItem)
+	{
+		DefaultedList<ItemStack> main = inventory.main;
+
+		/* Array Structure:	0: enchantLevel,	1: slotIndex */
+		final int[] itemSlots = {-1, -1}; // initializing everything to -1
+
+		// TODO: if isTool || isMeleeItem -> Item Material check so we know what to skip
+
+		/*
+		Order of search !Excludes any crafting/armor/offhand slot!:
+		0 - 8: Hot bar
+		9 - 35: Inventory from top left to bottom right, left to right, row for row.
+		 */
+		for (int i = 0; i < main.size(); i++)
+		{
+			ItemStack stack = main.get(i);
+			/* skip the currently selected sameItemSlot as it might have a broken item in it, weird stuff | only
+			needed for
+			the item broken check though, never happens anywhere else - probably just the fact my injection point-/s
+			SUCK lmao */
+			if (i == inventory.selectedSlot)
+				continue;
+			/*
+			Compares the old Item with the item in the sameItemSlot i, if they have the same name, they are at least
+			of the
+			same type.
+			 */
+			else if (getItemIdentifier(oldItem).equals(getItemIdentifier(stack.getItem())))
+			{
+				int enchantLevel;
+				// We can assume that if we get here, the item we are looking at is actually a tool too
+				if (isTool)
+				{
+					/* Is the current tool we are replacing a silk touch one? */
+					if (isSilkTouch)
+					{
+						/* If so, is the one we are looking at silk touch too? */
+						if (EnchantmentHelper.hasSilkTouch(stack))
+							enchantLevel = EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack);
+						else
+							continue; // no? next!
+					}
+					else if (!EnchantmentHelper.hasSilkTouch(stack))
+						enchantLevel = EnchantmentHelper.getLevel(Enchantments.EFFICIENCY, stack);
+					else
+						continue;
+				}
+				else if (isMeleeItem)
+				{
+					enchantLevel = EnchantmentHelper.getLevel(Enchantments.SHARPNESS, stack);
+				}
+//				else if (isRangedItem)
+//				{
+//					// TODO: What do I even look for in (cross-)bows? Power? Punch?
+//				}
+				else
+				{
+					return i;
+				}
+
+				if (itemSlots[0] < enchantLevel)
+				{
+					itemSlots[0] = enchantLevel;
+					itemSlots[1] = i;
+				}
+
+			}
+
+		}
+
+		/* should be either -1 still or actually be a slot in the inventory that has the next best alternative to the
+		 broken/used item */
+		return itemSlots[1];
+	}
+
+	/**
+	 * @param item The {@link Item} to return the {@link Identifier} of
+	 * @return The identifier of the given item
+	 */
+	public static @NotNull Identifier getItemIdentifier(Item item)
 	{
 		return Registries.ITEM.getId(item);
 	}
--- src/main/java/de/confue/autoitemreplace/mixin/ItemStackMixin.java
+++ src/main/java/de/confue/autoitemreplace/mixin/ItemStackMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtList;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -20,6 +19,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 
 @Mixin({ItemStack.class})
@@ -35,30 +35,6 @@ public class ItemStackMixin
 	 */
 	private static EquipmentSlot lastUsedEquipmentSlot = EquipmentSlot.MAINHAND;
 
-	private static Item brokenItem = null;
-
-	/*
-	preItemBrokenCallback: Injected at the head of the method before any code was executed, clears the brokenItem
-	field.
-
-	itemBrokenCallback: Only called if the item was actually broken with the received damage stores the item in
-	question in the brokenItem field.
-
-	postItemBrokenCallback: Injected at any given return point of this method. If the brokenItem field is not null it
-	will go ahead and switch the broken tool/weapon
-	 */
-	@Inject(method = {"damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V"}, at = {@At(value =
-			"HEAD", target = "Lnet/minecraft/item/ItemStack;decrement(I)V")}, locals = LocalCapture.CAPTURE_FAILSOFT)
-	private <T extends LivingEntity> void preItemBrokenCallback(int amount, T entity, Consumer<T> breakCallback,
-																CallbackInfo ci)
-	{
-		System.out.println("ItemStackMixin.preItemBrokenCallback");
-		NbtList nbtElements = ((ItemStack)(Object)this).getEnchantments();
-		System.out.println("nbtElements.size() = " + nbtElements.size());
-
-//		brokenItem = null;
-	}
-
 	@Inject(method = {"damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V"}, at = {@At(value =
 			"INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V")}, locals = LocalCapture.CAPTURE_FAILSOFT)
 	private <T extends LivingEntity> void itemBrokenCallback(int amount, T entity, Consumer<T> breakCallback,
@@ -66,33 +42,16 @@ public class ItemStackMixin
 	{
 //		brokenItem = item;
 
-		if (item == null || brokenItem instanceof ArmorItem)
+		if (item == null || item instanceof ArmorItem)
 			return;
 
 		// Retrieves the player inventory
-		int slot = AutoItemSwitchUtilities.getSlotWithMatchingOrEqualItemFromInventory(((ItemStack)(Object)this));
+		int slot = AutoItemSwitchUtilities.getSlotWithMatchingOrEqualItemFromInventory(((ItemStack) (Object) this));
 		if (slot == -1)
 			return;
 
 		// Swap the found item with the currently selected hot bar slot
-		MinecraftClient.getInstance().interactionManager.pickFromInventory(slot);
-	}
-
-	@Inject(method = {"damage(ILnet/minecraft/entity/LivingEntity;Ljava/util/function/Consumer;)V"}, at = {@At(value =
-			"RETURN", target = "Lnet/minecraft/item/ItemStack;decrement(I)V")}, locals = LocalCapture.CAPTURE_FAILSOFT)
-	private <T extends LivingEntity> void postItemBrokenCallback(int amount, T entity, Consumer<T> breakCallback,
-																 CallbackInfo ci)
-	{
-//		if (brokenItem == null || brokenItem instanceof ArmorItem)
-//			return;
-//
-//		// Retrieves the player inventory
-//		int slot = AutoItemSwitchUtilities.getSlotWithMatchingOrEqualItemFromInventory(((ItemStack)(Object)this));
-//		if (slot == -1)
-//			return;
-//
-//		// Swap the found item with the currently selected hot bar slot
-//		MinecraftClient.getInstance().interactionManager.pickFromInventory(slot);
+		Objects.requireNonNull(MinecraftClient.getInstance().interactionManager).pickFromInventory(slot);
 	}
 
 	/*
@@ -104,7 +63,7 @@ public class ItemStackMixin
 	private void use(World world, PlayerEntity user, Hand hand,
 					 CallbackInfoReturnable<TypedActionResult<ItemStack>> cir)
 	{
-		if (!(user instanceof ClientPlayerEntity))
+		if (!(user instanceof ClientPlayerEntity) || user.isSpectator())
 			return;
 
 		lastUsedEquipmentSlot = hand == Hand.MAIN_HAND ? EquipmentSlot.MAINHAND :
@@ -116,7 +75,7 @@ public class ItemStackMixin
 	@Inject(method = {"finishUsing"}, at = {@At(value = "RETURN")})
 	private void finishUsing(World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir)
 	{
-		if (!(user instanceof ClientPlayerEntity))
+		if (!(user instanceof ClientPlayerEntity) || user.isSpectator())
 			return;
 
 		ItemStack itemStack = user.getEquippedStack(lastUsedEquipmentSlot);
@@ -125,13 +84,11 @@ public class ItemStackMixin
 			return;
 
 		int slot = AutoItemSwitchUtilities.getSlotWithMatchingOrEqualItemFromInventory(lastUsedItemStack);
+
 		if (slot == -1)
 			return;
 
-//		inventory.swapSlotWithHotbar(slot);
-		MinecraftClient.getInstance().interactionManager.pickFromInventory(slot);
+		Objects.requireNonNull(MinecraftClient.getInstance().interactionManager).pickFromInventory(slot);
 	}
-
-
 
 }
